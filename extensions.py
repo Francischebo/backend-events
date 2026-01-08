e5d99cc9da6b7cc9c7922dd3b1914069b80316ba
Francis Chebo Ividah <86152359+Francischebo@users.noreply.github.com>
2026-01-09 00:48:16 +0300

    Implement CustomJSONProvider for BSON ObjectId
    Add CustomJSONProvider to handle ObjectId serialization

--- extensions.py
+++ extensions.py
@@ -7,6 +7,16 @@ from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO
 from flask_bcrypt import Bcrypt
 
+from flask.json.provider import DefaultJSONProvider
+from bson import ObjectId
+
+class CustomJSONProvider(DefaultJSONProvider):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super().default(obj)
+
+
 # Initialize extensions
 class _MongoWrapper:
 	"""Wrapper around Flask-PyMongo to provide a reliable `db` attribute.
